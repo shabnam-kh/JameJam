eb90eca399d43b0b02139235c32493454fa40b51
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-07-05 17:18:27 +0430

    handlebar+chiller Info+Mongo DB

--- jamejamApp/www/js/chillerInfoHandle.js
+++ jamejamApp/www/js/chillerInfoHandle.js
@@ -1,16 +1,23 @@
 /**
  * Created by klappo on 7/5/15.
  */
+
 $(function () {
   // Grab the template script
-  var theTemplateScript = $(".chillerInfoTemp").html();
+        var chillerInfo=sessionStorage.getItem("chillerInfo");
+        console.log("info "+chillerInfo);
+        var data=JSON.parse(chillerInfo);
+          var theTemplateScript = $(".chillerInfoTemp").html();
 
   // Compile the template
   var theTemplate = Handlebars.compile(theTemplateScript);
 
   // Define our data object
   var context={
-    "tell": "221"
+    "tell": data.tell,
+      "chiller":data.ownerName,
+      "complex":data.complexName,
+      "name":data.chillerName
   };
 
   // Pass our data to the template
@@ -19,3 +26,4 @@ $(function () {
   // Add the compiled html to the page
   $('.infoBox').html(theCompiledHtml);
 });
+
