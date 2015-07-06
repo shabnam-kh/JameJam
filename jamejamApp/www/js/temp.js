f2d33a8aa682641f87d955ae0d72d83a3dc7d8d7
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-07-06 13:48:05 +0430

    edit server endpoints + MongoDB connect Method

--- jamejamApp/www/js/temp.js
+++ jamejamApp/www/js/temp.js
@@ -9,7 +9,7 @@ var reqFlag=false;
         //    console.log("call showhex");
         //    showHex(data);
         //})
-        //    $.get(baseUrl+"/chillerInfo",function(a,b,c){
+        //    $.get(baseUrl+"/data",function(a,b,c){
         //        var aa=JSON.parse(a);
         //        var t=aa.temp;
         //        var indx="1";
