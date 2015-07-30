ee4193094a443e70a70e933347b6bcb9721a695e
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-07-30 14:55:54 +0430

    update radio buttons status by data

--- jamejamApp/www/js/function.js
+++ jamejamApp/www/js/function.js
@@ -5,3 +5,38 @@ function getChillerNumber(str){
     var temp=str.split(" ");
     return temp[1]
 }
+
+    function getSessionData(callback){
+        var sID=sessionStorage.getItem('secureID')
+        console.log("temp active chiller "+sessionStorage.getItem('activeChiller'));
+        var activeChiller=getChillerNumber(sessionStorage.getItem('activeChiller'));
+        callback(sID,activeChiller);
+    }
+
+ function sendReq(callFail,callSuccess) {
+     getSessionData(function (Sid,activeChill) {
+     //        $.get(baseUrl + "/chillerData", {sID:Sid,id:activeChill}, function (data, textStatus, jqXHR) {
+     //    console.log("send request to /chillerData");
+     //   callSuccess(data)
+     //})
+         $.ajax(
+             {
+                 url: serverURL + "/chillerData",
+                    data: {sID:Sid,id:activeChill},
+                 timeout: 2000,
+                 success: function (data, textStatus, jqXHR) {
+                     console.log("ajax req " + textStatus);
+                     console.log("ajax req data " + data);
+                     callSuccess(data);
+                 },
+                 error: function (jqXHR, textStatus, errorThrown)
+                 {
+                     console.log("ajax req fail" + textStatus);
+                     console.log("ajax req error " + errorThrown);
+                     callFail();
+                 }
+     }
+         )
+     })
+
+ }
