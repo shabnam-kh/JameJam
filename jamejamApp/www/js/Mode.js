e5a933e625c1420e274ee9547895d7783edad293
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-08-11 10:50:41 +0430

    edit setting mode (new data)

--- jamejamApp/www/js/Mode.js
+++ jamejamApp/www/js/Mode.js
@@ -42,30 +42,6 @@ var storeData=sessionStorage.getItem('chillerData');
          }
 
      },2000)
-     //
-     //function sendReq(callFail,callSuccess){
-     //
-     //    $.ajax(
-     //        {
-     //            url: serverURL + "/chillerData",
-     //               data: {sID:Sid,id:activeChill},
-     //            timeout: 2000,
-     //            success: function (data, textStatus, jqXHR) {
-     //                console.log("ajax req " + textStatus);
-     //                console.log("ajax req data " + data);
-     //                callSuccess(data);
-     //            },
-     //            error: function (jqXHR, textStatus, errorThrown)
-     //            {
-     //                console.log("ajax req fail" + textStatus);
-     //                console.log("ajax req error " + errorThrown);
-     //                callFail();
-     //            }
-     //}
-     //    )
-     //
-     //
-     //}
  function sendReq(callFail,callSuccess) {
      getSessionData(function (Sid,activeChill) {
      //        $.get(baseUrl + "/chillerData", {sID:Sid,id:activeChill}, function (data, textStatus, jqXHR) {
