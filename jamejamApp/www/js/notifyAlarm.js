d0b58f9535d1883d715ac44a7b3f60bac3381763
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-09-23 22:31:29 +0330

    change alarm condition

--- jamejamApp/www/js/notifyAlarm.js
+++ jamejamApp/www/js/notifyAlarm.js
@@ -4,34 +4,11 @@
 
 document.addEventListener('deviceready', function () {
 
+    checkAlarm();
     var sendReqToServer = setInterval(function () {
 
-        var alarm=sessionStorage.getItem("alarm");
-        console.log("storage alarm is " +alarm);
-        if(alarm=="active"){
-            console.log("call sendReq");
-        sendReq(function () {
-        }, function (data) {
-            console.log("notification alarm page");
-            var JsonData = JSON.parse(data);
 
-            var alarmData = JsonData.Alarm;
-            sessionStorage.setItem("alarmData",alarmData);
-            var digi = alarmData.split(",");
-            console.log("alarm data is  " + digi);
-            var flag = digi.indexOf("1");
-            if (flag == -1) {
-                console.log("NO ALARM");
-            } else {
-                console.log("ALARM NOTIFICATION");
-                notifyAlarm();
-            }
-        });
-        }else{
-            console.log("alarm deactivated by user");
-        }
-
-
+        checkOldAlarm();
 
     }, 5000);
 
@@ -41,11 +18,11 @@ document.addEventListener('deviceready', function () {
     $("#alarmCheck").click(function () {
         if ($("#alarmCheck").is(":checked")) {
             console.log("notification alarm is active");
-            sessionStorage.setItem("alarm","active");
+            sessionStorage.setItem("alarm", "active");
         } else {
             console.log("deactive notification alarm");
             sessionStorage.removeItem("alarm");
-            cordova.plugins.notification.local.cancelAll(function(){
+            cordova.plugins.notification.local.cancelAll(function () {
                 alert("cancel alarm notification");
             });
         }
@@ -61,6 +38,80 @@ document.addEventListener('deviceready', function () {
             text: "WARNING !!! chiller has alarm !!!"
         });
 
+    }
+
+    function checkAlarm() {
+        var alarm = sessionStorage.getItem("alarm");
+        console.log("storage alarm is " + alarm);
+        if (alarm == "active") {
+            console.log("call sendReq");
+            sendReq(function () {
+            }, function (data) {
+                console.log("notification alarm page");
+                var JsonData = JSON.parse(data);
+
+                var alarmData = JsonData.Alarm;
+                var digi = alarmData.split(",");
+                sessionStorage.setItem("alarmData", digi);
+                console.log("alarm data is  " + digi);
+                var flag = digi.indexOf("1");
+                if (flag == -1) {
+                    console.log("NO ALARM");
+                } else {
+                    console.log("ALARM NOTIFICATION");
+                    notifyAlarm();
+                }
+            });
+        } else {
+            console.log("alarm deactivated by user");
+        }
+
+    }
+
+    function checkOldAlarm() {
+        var alarmOldData;
+        var alarm = sessionStorage.getItem("alarm");
+        console.log("storage alarm is " + alarm);
+        if (alarm == "active") {
+            console.log("call sendReq");
+            sendReq(function () {
+            }, function (data) {
+                console.log("notification alarm page");
+                var JsonData = JSON.parse(data);
+                var alarmData = JsonData.Alarm;
+                var digi = alarmData.split(",");
+                console.log("alarm data is  " + digi);
+                getOldDataAlarm(function (dd) {
+                    alarmOldData = dd;
+                    sessionStorage.setItem("alarmData", digi);
+                    for (var i = 0; i < digi.length; i++) {
+                        var flag = digi.indexOf("1");
+                        if (flag == -1) {
+                            console.log("NO ALARM");
+                        } else {
+                            console.log("ALARM NOTIFICATION");
+                            if (digi[flag] == alarmOldData[flag]) {
+                                console.log("alarm expired");
+                            } else {
+                                notifyAlarm();
+                                break;
+                            }
+
+                        }
+                    }
+                });
+
+
+            });
+        } else {
+            console.log("alarm deactivated by user");
+        }
+
+    }
+
+    function getOldDataAlarm(callback) {
+        var data = sessionStorage.getItem("alarmData");
+        callback(data);
     }
 
 }, false);
