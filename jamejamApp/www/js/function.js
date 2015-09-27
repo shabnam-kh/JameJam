48bec68e9fb471f992c3cc7cdcb779f95a46eb9f
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-09-27 22:28:56 +0330

    alarm indicator light

--- jamejamApp/www/js/function.js
+++ jamejamApp/www/js/function.js
@@ -9,16 +9,15 @@ function getChillerNumber(str) {
 function getSessionData(callback) {
     var sID = sessionStorage.getItem('secureID')
     console.log("temp active chiller " + sessionStorage.getItem('activeChiller'));
-    var activeChiller = getChillerNumber(sessionStorage.getItem('activeChiller'));
+    try{
+        var activeChiller = getChillerNumber(sessionStorage.getItem('activeChiller'));
+    }catch(e){}
+
     callback(sID, activeChiller);
 }
 
 function sendReq(callFail, callSuccess) {
     getSessionData(function (Sid, activeChill) {
-        //        $.get(baseUrl + "/chillerData", {sID:Sid,id:activeChill}, function (data, textStatus, jqXHR) {
-        //    console.log("send request to /chillerData");
-        //   callSuccess(data)
-        //})
         $.ajax(
             {
                 url: serverURL + "/chillerData",
@@ -39,6 +38,31 @@ function sendReq(callFail, callSuccess) {
     })
 
 }
+
+function reqChillerData(callFail, callSuccess, chID) {
+    console.log("call reqChillerData function");
+    getSessionData(function (Sid, activeChill) {
+        $.ajax(
+            {
+                url: serverURL + "/chillerData",
+                data: {sID: Sid, id: chID},
+                timeout: 2000,
+                success: function (data, textStatus, jqXHR) {
+                    console.log("ajax req " + textStatus);
+                    console.log("ajax req data " + data);
+                    callSuccess(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log("ajax req fail" + textStatus);
+                    console.log("ajax req error " + errorThrown);
+                    callFail(errorThrown);
+                }
+            }
+        )
+    })
+
+}
+
 function showDate() {
     var d = new Date(),
         seconds = d.getSeconds().toString().length == 1 ? '0' + d.getSeconds() : d.getSeconds(),
