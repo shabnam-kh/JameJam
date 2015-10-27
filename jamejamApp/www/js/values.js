6e065d662aa691957f8c17bc4f2d083c35915e59
shabnam_kh <shabnam.khodadadi@gmail.com>
2015-10-27 22:10:16 +0330

    add upper + lower bound for 11 tempreture of each chiller

---
+++ jamejamApp/www/js/values.js
@@ -0,0 +1,10 @@
+/**
+ * Created by klappo on 10/26/15.
+ */
+
+//set temperature bounds
+
+chiller01=[[1,24],[2,56],[3,57],[8,98],[5,67],[5,67],[5,67],[5,67],[5,67],[5,67],[5,67]]
+chTempBounds={'01':chiller01}
+
+
